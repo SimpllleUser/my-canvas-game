f3fc040906b4e660b0e34058ca023d0b76d1252e
Vitalii <pecheritsa.vv@gmail.com>
2025-08-22 22:46:56 +0300

    doc: add movement cube by press key

--- src/entities/Cube.ts
+++ src/entities/Cube.ts
@@ -1,5 +1,6 @@
 import { Canvas } from "./Canvas.ts";
 import type { IGameObject } from "../types/GameObject.ts";
+import type { IPosition } from "../types/Main.ts";
 
 const ELEMENT = {
   width: 15,
@@ -7,63 +8,96 @@ const ELEMENT = {
   color: "blue",
 };
 
+interface Directions {
+  Up: boolean;
+  Down: boolean;
+  Left: boolean;
+  Right: boolean;
+}
+
 export class Cube extends Canvas implements IGameObject {
   element = ELEMENT;
 
+  velocity: number;
+  position: IPosition;
+  direction: Directions;
+
   constructor() {
     super();
+
+    this.velocity = 10;
+    this.position = {
+      x: 0,
+      y: 0,
+    };
+    this.direction = {
+      Up: false,
+      Down: false,
+      Left: false,
+      Right: false,
+    };
+
+    addEventListener("keydown", (event) => {
+      this.moveRight(event, true);
+      this.moveLeft(event, true);
+      this.moveUp(event, true);
+      this.moveBottom(event, true);
+      this.setMoveDirection();
+    });
+    addEventListener("keyup", (event) => {
+      this.moveRight(event, false);
+      this.moveLeft(event, false);
+      this.moveUp(event, false);
+      this.moveBottom(event, false);
+      this.setMoveDirection();
+    });
   }
 
   draw() {
     this.ctx.save();
     this.ctx.fillStyle = this.element.color;
-    this.ctx.fillRect(0, 0, this.element.width, this.element.height);
+    this.ctx.fillRect(
+      this.position.x,
+      this.position.y,
+      this.element.width,
+      this.element.height,
+    );
     this.ctx.restore();
   }
+
+  setMoveDirection() {
+    if (this.direction.Right) {
+      this.position.x += this.velocity;
+    }
+    if (this.direction.Left) {
+      this.position.x -= this.velocity;
+    }
+    if (this.direction.Up) {
+      this.position.y -= this.velocity;
+    }
+    if (this.direction.Down) {
+      this.position.y += this.velocity;
+    }
+  }
+
+  moveRight({ key }: KeyboardEvent, state: boolean) {
+    if (key === "ArrowRight" || key.toLowerCase() === "d") {
+      this.direction.Right = state;
+    }
+  }
+  moveLeft({ key }: KeyboardEvent, state: boolean) {
+    if (key === "ArrowLeft" || key.toLowerCase() === "a") {
+      this.direction.Left = state;
+    }
+  }
+  moveUp({ key }: KeyboardEvent, state: boolean) {
+    if (key === "ArrowUp" || key.toLowerCase() === "w") {
+      this.direction.Up = state;
+    }
+  }
+  moveBottom({ key }: KeyboardEvent, state: boolean) {
+    if (key === "ArrowDown" || key.toLowerCase() === "s") {
+      this.direction.Down = state;
+    }
+  }
 }
-// const keys = {
-//   ArrowUp: false,
-//   ArrowDown: false,
-//   ArrowLeft: false,
-//   ArrowRight: false
-// };
-// function setupArrowKeysListener(callback: (direction: { x: number; y: number }) => void) {
-//   document.addEventListener('keydown', (event) => {
-//     if (event.code in keys) {
-//       event.preventDefault();
-//       (keys as any)[event.code] = true;
-//
-//       // Викликаємо callback з поточним напрямком
-//       const direction = {
-//         x: (keys.ArrowRight ? 1 : 0) - (keys.ArrowLeft ? 1 : 0),
-//         y: (keys.ArrowDown ? 1 : 0) - (keys.ArrowUp ? 1 : 0)
-//       };
-//
-//       callback(direction);
-//     }
-//   });
-//
-//   document.addEventListener('keyup', (event) => {
-//     if (event.code in keys) {
-//       event.preventDefault();
-//       (keys as any)[event.code] = false;
-//
-//       // Викликаємо callback з поточним напрямком
-//       const direction = {
-//         x: (keys.ArrowRight ? 1 : 0) - (keys.ArrowLeft ? 1 : 0),
-//         y: (keys.ArrowDown ? 1 : 0) - (keys.ArrowUp ? 1 : 0)
-//       };
-//
-//       callback(direction);
-//     }
-//   });
-// }
-//
-// // Приклад використання
-// setupArrowKeysListener((direction) => {
-//   if (direction.x !== 0 || direction.y !== 0) {
-//     console.log(`Рух: x=${direction.x}, y=${direction.y}`);
-//   } else {
-//     console.log('Зупинка');
-//   }
-// });
