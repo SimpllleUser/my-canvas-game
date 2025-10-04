799ebf58a85530e3ed65ffc6ef1fd41392375031
Vitalii <pecheritsa.vv@gmail.com>
2025-10-04 23:12:20 +0300

    feat: extract player movement logic into PlayerMovement class
    - Moved movement-related logic from `Cube` to a new `PlayerMovement` class for better modularity.
    - Updated `Cube` to use `PlayerMovement` for handling direction and position updates.
    - Improved code organization by encapsulating directional key handling and movement constraints.

--- src/entities/Cube.ts
+++ src/entities/Cube.ts
@@ -3,6 +3,7 @@ import type { IGameObject } from "../types/GameObject.ts";
 import type { IPosition } from "../types/Main.ts";
 import { Sight } from "./Sight.ts";
 import type { BaseWeapon } from "./BaseWeapon.ts";
+import { PlayerMovement } from "./PlayerMovement.ts";
 
 const ELEMENT = {
   width: 50,
@@ -10,64 +11,30 @@ const ELEMENT = {
   color: "blue",
 };
 
-const VELOCITY = 10;
 const getBasePosition = () => ({ x: 0, y: 0 });
-
-interface Directions {
-  Up: boolean;
-  Down: boolean;
-  Left: boolean;
-  Right: boolean;
-}
-
-enum KeyCodes {
-  ArrowRight = 68,
-  ArrowLeft = 65,
-  ArrowUp = 87,
-  ArrowDown = 83,
-}
 
 export class Cube extends Canvas implements IGameObject {
   element = ELEMENT;
-  velocity: number;
   position: IPosition;
-  direction: Directions;
   mousePosition: IPosition;
   center: IPosition;
   weapon: BaseWeapon;
   sight: Sight;
+  playerMovement: PlayerMovement;
 
   constructor(weapon: BaseWeapon) {
     super();
-    this.velocity = VELOCITY;
     this.position = getBasePosition();
-    this.direction = { Up: false, Down: false, Left: false, Right: false };
     this.mousePosition = getBasePosition();
     this.center = getBasePosition();
     this.sight = new Sight();
     this.initEventListeners();
     this.animate();
     this.weapon = weapon;
+    this.playerMovement = new PlayerMovement(ELEMENT);
   }
 
   initEventListeners() {
-    addEventListener("keydown", (event) => {
-      const { keyCode } = event;
-      this.moveRight(keyCode, true);
-      this.moveLeft(keyCode, true);
-      this.moveUp(keyCode, true);
-      this.moveBottom(keyCode, true);
-    });
-
-    addEventListener("keyup", (event) => {
-      const { keyCode } = event;
-
-      this.moveRight(keyCode, false);
-      this.moveLeft(keyCode, false);
-      this.moveUp(keyCode, false);
-      this.moveBottom(keyCode, false);
-    });
-
     this.canvas.addEventListener("mousemove", ({ x, y }: MouseEvent) => {
       const { scale } = this.getData();
       const canvasX = x / scale;
@@ -129,7 +96,7 @@ export class Cube extends Canvas implements IGameObject {
     this.ctx.save();
 
     this.ctx.fillStyle = this.element.color;
-    this.canMove() && this.setMoveDirection();
+    this.playerMovement?.setMoveDirection(this.position);
     this.drawSight();
     this.ctx.fillRect(
       this.position.x,
@@ -146,62 +113,5 @@ export class Cube extends Canvas implements IGameObject {
   animate() {
     this.draw();
     requestAnimationFrame(() => this.animate());
-  }
-
-  canMove() {
-    const canRight =
-      this.position.x + this.element.width + this.velocity < this.canvas.width;
-    const canLeft = this.position.x > 0;
-    const canUp = this.position.y > 0;
-    const canDown =
-      this.position.y + this.element.height + this.velocity <
-      this.canvas.height;
-
-    return {
-      canRight,
-      canLeft,
-      canUp,
-      canDown,
-    };
-  }
-
-  setMoveDirection() {
-    const { canRight, canLeft, canUp, canDown } = this.canMove();
-    if (this.direction.Right && canRight) {
-      this.position.x += this.velocity;
-    }
-    if (this.direction.Left && canLeft) {
-      this.position.x -= this.velocity;
-    }
-    if (this.direction.Up && canUp) {
-      this.position.y -= this.velocity;
-    }
-    if (this.direction.Down && canDown) {
-      this.position.y += this.velocity;
-    }
-  }
-
-  moveRight(keyCode: KeyCodes, state: boolean) {
-    if (keyCode === KeyCodes.ArrowRight) {
-      this.direction.Right = state;
-    }
-  }
-
-  moveLeft(keyCode: KeyCodes, state: boolean) {
-    if (keyCode === KeyCodes.ArrowLeft) {
-      this.direction.Left = state;
-    }
-  }
-
-  moveUp(keyCode: KeyCodes, state: boolean) {
-    if (keyCode === KeyCodes.ArrowUp) {
-      this.direction.Up = state;
-    }
-  }
-
-  moveBottom(keyCode: KeyCodes, state: boolean) {
-    if (keyCode === KeyCodes.ArrowDown) {
-      this.direction.Down = state;
-    }
   }
 }
---
+++ src/entities/PlayerMovement.ts
@@ -0,0 +1,117 @@
+import type { IPosition } from "../types/Main.ts";
+import { Canvas } from "./Canvas.ts";
+
+enum KeyCodes {
+  ArrowRight = 68,
+  ArrowLeft = 65,
+  ArrowUp = 87,
+  ArrowDown = 83,
+}
+
+interface IElement {
+  width: number;
+  height: number;
+  color: string;
+}
+// const ELEMENT = {
+//   width: 50,
+//   height: 50,
+//   color: "blue",
+// };
+
+interface Directions {
+  Up: boolean;
+  Down: boolean;
+  Left: boolean;
+  Right: boolean;
+}
+const VELOCITY = 10;
+
+export class PlayerMovement extends Canvas {
+  direction: Directions;
+  velocity: number;
+  element: IElement;
+
+  constructor(element: IElement) {
+    super();
+    this.element = element;
+    this.velocity = VELOCITY;
+    this.direction = { Up: false, Down: false, Left: false, Right: false };
+    this.initKeyEventListener();
+  }
+
+  initKeyEventListener() {
+    addEventListener("keydown", (event) => {
+      const { keyCode } = event;
+      this.moveRight(keyCode, true);
+      this.moveLeft(keyCode, true);
+      this.moveUp(keyCode, true);
+      this.moveBottom(keyCode, true);
+    });
+
+    addEventListener("keyup", (event) => {
+      const { keyCode } = event;
+
+      this.moveRight(keyCode, false);
+      this.moveLeft(keyCode, false);
+      this.moveUp(keyCode, false);
+      this.moveBottom(keyCode, false);
+    });
+  }
+
+  moveRight(keyCode: KeyCodes, state: boolean) {
+    if (keyCode === KeyCodes.ArrowRight) {
+      this.direction.Right = state;
+    }
+  }
+
+  moveLeft(keyCode: KeyCodes, state: boolean) {
+    if (keyCode === KeyCodes.ArrowLeft) {
+      this.direction.Left = state;
+    }
+  }
+
+  moveUp(keyCode: KeyCodes, state: boolean) {
+    if (keyCode === KeyCodes.ArrowUp) {
+      this.direction.Up = state;
+    }
+  }
+
+  moveBottom(keyCode: KeyCodes, state: boolean) {
+    if (keyCode === KeyCodes.ArrowDown) {
+      this.direction.Down = state;
+    }
+  }
+
+  setMoveDirection(position: IPosition) {
+    const { canRight, canLeft, canUp, canDown } = this.canMove(position);
+    if (this.direction.Right && canRight) {
+      position.x += this.velocity;
+    }
+    if (this.direction.Left && canLeft) {
+      position.x -= this.velocity;
+    }
+    if (this.direction.Up && canUp) {
+      position.y -= this.velocity;
+    }
+    if (this.direction.Down && canDown) {
+      position.y += this.velocity;
+    }
+  }
+
+  canMove(position: IPosition) {
+    const canRight =
+      position.x + this.element.width + this.velocity < this.canvas.width;
+    const canLeft = position.x > 0;
+    const canUp = position.y > 0;
+    const canDown =
+      position.y + this.element.height + this.velocity < this.canvas.height;
+
+    return {
+      canRight,
+      canLeft,
+      canUp,
+      canDown,
+    };
+  }
+}
